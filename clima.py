3157139a02b50bf96f6d008b65c51eb9dc5b04e5
mvalverdecad <mikivalverde1994@gmail.com>
2026-04-26 17:15:06 +0200

    Reto del clima funcionando

---
+++ clima.py
@@ -0,0 +1,8 @@
+temperatura = 10 # podemos variar esta temperatura
+if temperatura > 25:
+    print("Encender aire frío")
+elif temperatura < 15:
+    print("Encender calefacción")
+else:
+    print("temperatura perfecta")
+
